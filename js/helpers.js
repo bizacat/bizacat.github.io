cb559fbe18d8f6a47faa51add4aa295cd6541ce2
Bizaca, Tina <tbizaca@mobicorp.com>
2015-11-13 16:50:12 -0600

    tons of contennt, made tag clomade tag cloud static

--- js/helpers.js
+++ js/helpers.js
@@ -14,44 +14,45 @@ var tagListHyphenated = [];
 
 // Loop through each tag on page and create tag for it in tagCloudUl
 for (i=0; i < tagList.length; i++) {
-    var liTag = $('<li>');
-    var aTag = $('<a>');
-    tagCloudUl.append(liTag);
+    //var liTag = $('<li>');
+    //var aTag = $('<a>');
+    //tagCloudUl.append(liTag);
+    //
+    //liTag.append(aTag);
+    //aTag.addClass('tag').text(tagList[i]);
+    //
+    //// hyphenate the tags
+    //tagListHyphenated = tagList[i].replace(/\s+/g, '-');
+    //
+    //// add hyphenated version as id to the anchors
+    //aTag.attr('id', tagListHyphenated);
 
-    liTag.append(aTag);
-    aTag.addClass('tag').text(tagList[i]);
-
-    // hyphenate the tags
-    tagListHyphenated = tagList[i].replace(/\s+/g, '-');
-
-    // add hyphenated version as id to the anchors
-    aTag.attr('id', tagListHyphenated);
-
-function showAll() {
-    $('section.design-work').show();
-}
+    function showAll() {
+        $('section.design-work').show();
+    }
 
 // When clicking on a tag's anchor in the tagCloud
-    $('a.tag').click(function() {
+    $('a.tag').click(function () {
         // sections with matching tag
         var matchingSection = $('section.' + $(this).attr('id'));
 
-        if ( $(this).hasClass('selected') ) {
+        if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
             showAll();
         } else {
             // Add selected tag to the tagCloud item that was clicked
+            $('a.tag').removeClass('selected');
             $(this).addClass('selected');
             // hide all sections
             $('section.design-work').hide();
             // find the matching section(s) with this as a class and give them the class of show
-            matchingSection.show();
+            matchingSection.show().addClass('selected');
         }
     });
 }
-
-
-$('#showAll').click(function(){
-    showAll();
-    $('a.tag').removeClass('selected');
-});
+//
+//
+//$('#showAll').click(function(){
+//    showAll();
+//    $('a.tag').removeClass('selected');
+//});
