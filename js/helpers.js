b09082be846be54df004069db340210e16dcc867
Bizaca, Tina <tbizaca@mobicorp.com>
2015-11-16 13:55:06 -0600

    new lightbox

--- js/helpers.js
+++ js/helpers.js
@@ -14,19 +14,6 @@ var tagListHyphenated = [];
 
 // Loop through each tag on page and create tag for it in tagCloudUl
 for (i=0; i < tagList.length; i++) {
-    //var liTag = $('<li>');
-    //var aTag = $('<a>');
-    //tagCloudUl.append(liTag);
-    //
-    //liTag.append(aTag);
-    //aTag.addClass('tag').text(tagList[i]);
-    //
-    //// hyphenate the tags
-    //tagListHyphenated = tagList[i].replace(/\s+/g, '-');
-    //
-    //// add hyphenated version as id to the anchors
-    //aTag.attr('id', tagListHyphenated);
-
     function showAll() {
         $('section.design-work').show();
     }
@@ -35,24 +22,25 @@ for (i=0; i < tagList.length; i++) {
     $('a.tag').click(function () {
         // sections with matching tag
         var matchingSection = $('section.' + $(this).attr('id'));
+        var matchingText = $(this).text();
 
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
+            // Remove the filtered class in the content that was matched/filtered above
+            $('.filtered').removeClass('filtered');
             showAll();
         } else {
+            // Highlight the tag in the content that is being matched/filtered above
+            $('span.tag').filter(function() {
+                return $(this).text() === matchingText;
+            }).addClass("filtered");
             // Add selected tag to the tagCloud item that was clicked
             $('a.tag').removeClass('selected');
             $(this).addClass('selected');
             // hide all sections
             $('section.design-work').hide();
-            // find the matching section(s) with this as a class and give them the class of show
-            matchingSection.show().addClass('selected');
+            // find the matching section(s) with this as a class and show them
+            matchingSection.show();
         }
     });
 }
-//
-//
-//$('#showAll').click(function(){
-//    showAll();
-//    $('a.tag').removeClass('selected');
-//});
